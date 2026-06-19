aa8d4c194f1644568a33cb42a3959b9f9f207a83
rajkishorock-arch <rajkishorock@gmail.com>
2026-06-19 07:29:56 +0530

    Support SSL connections for cloud databases in SQLAlchemy

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+connect_args = {}
+if "localhost" not in DATABASE_URL and "127.0.0.1" not in DATABASE_URL:
+    connect_args = {
+        "ssl_verify_cert": False,
+        "ssl_verify_identity": False
+    }
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
